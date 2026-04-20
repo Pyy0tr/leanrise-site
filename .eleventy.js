fcf80dfd904431b9e46abe198e379d3c432400fa
Pyyotr <pierre45240@gmail.com>
2026-04-20 11:56:57 +0200

    fix: new icone and stop auto scroll to the middle

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("src/favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
 
   return {
     dir: {
